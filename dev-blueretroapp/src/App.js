81e6bef23092818e48f498547122535b31752ade
delislej <delislej@gmail.com>
2022-04-07 13:14:42 -0700

    tesing react gamepad

--- dev-blueretroapp/src/App.js
+++ dev-blueretroapp/src/App.js
@@ -1,23 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import { useGamepads } from 'react-gamepads';
 
 function App() {
+  const [gamepads, setGamepads] = useState({});
+  useGamepads(gamepads => setGamepads(gamepads));
+
+  const gamepadDisplay = Object.keys(gamepads).map(gamepadId => {
+    console.log("displaying gamepad", gamepads[gamepadId]);
+    return (
+      <div>
+        <h2>{gamepads[gamepadId].id}</h2>
+        {gamepads[gamepadId].buttons &&
+          gamepads[gamepadId].buttons.map((button, index) => (
+            <div>
+              {index}: {button.pressed ? 'True' : 'False'}
+            </div>
+          ))
+        }
+
+<h2>{"axes"}</h2>
+        {gamepads[gamepadId].buttons &&
+          gamepads[gamepadId].axes.map((axes, index) => (
+            <div>
+              {index}: {axes > .3 || axes < -.3 ? axes : ""}
+            </div>
+          ))
+        }
+      </div>
+    );
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Gamepads">
+      <h1>Gamepads</h1>
+      {gamepadDisplay}
     </div>
   );
 }
